98f9929f5417d49ccce5d58b91c1f324c9c0b7d1
gv4383 <gregory.vargas18@gmail.com>
2021-08-15 10:12:33 -0500

    Created constants for colors and text sizes

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,21 @@
+export const BUTTON_TEXT_SIZE = {
+  SMALL: 'sm',
+  MEDIUM: 'md',
+  LARGE: 'lg',
+};
+
+export const COLORS = {
+  DARK: 'dark',
+  GREEN: 'green',
+  RED: 'red',
+};
+
 export const ERROR_MESSAGES = {
   UNKOWN_ERROR: 'There was an unknown error.',
+};
+
+export const TEXT_SIZE = {
+  SMALL: 'sm',
+  MEDIUM: 'md',
+  LARGE: 'lg',
 };
